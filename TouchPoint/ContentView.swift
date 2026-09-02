23047bbea3f0fc656507bb8a9652f4b757d63fe7
Alex <avoronovski@gmail.com>
2026-09-02 15:31:32 -0700

    Initial Commit

---
+++ TouchPoint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TouchPoint
+//
+//  Created by Aleksei Voronovskii on 9/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TouchPoint/TouchPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchPointApp.swift
+//  TouchPoint
+//
+//  Created by Aleksei Voronovskii on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TouchPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
